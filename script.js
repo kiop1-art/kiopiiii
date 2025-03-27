de9771c17ba8e6f27d6c4e062bf05097728c55ef
kiop1_art <kiopiplay@ya.ru>
2025-03-28 01:58:14 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const audioPlayer = document.getElementById('audioPlayer');
+let currentTrackIndex = 0;
+const tracks = [
+    'audio/track1.mp3',
+    'audio/track2.mp3',
+    'audio/track3.mp3',
+    'audio/track4.mp3',
+    'audio/track5.mp3',
+    'audio/track6.mp3',
+    'audio/track7.mp3',
+    'audio/track8.mp3',
+    'audio/track9.mp3',
+    'audio/track10.mp3'
+];
+
+function playPause() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        document.getElementById('_40_20_icons8-_____-50_1').style.opacity = '0.5'; // Эффект нажатия
+    } else {
+        audioPlayer.pause();
+        document.getElementById('_40_20_icons8-_____-50_1').style.opacity = '1'; // Эффект отжатия
+    }
+}
+
+function nextTrack() {
+    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+    audioPlayer.src = tracks[currentTrackIndex];
+    audioPlayer.play();
+    document.getElementById('_40_21_icons8-_____-50_1').style.opacity = '0.5'; // Эффект нажатия
+    setTimeout(() => {
+        document.getElementById('_40_21_icons8-_____-50_1').style.opacity = '1'; // Эффект отжатия
+    }, 100);
+}
+
+function prevTrack() {
+    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+    audioPlayer.src = tracks[currentTrackIndex];
+    audioPlayer.play();
+    document.getElementById('_40_22_icons8-_____-50_2').style.opacity = '0.5'; // Эффект нажатия
+    setTimeout(() => {
+        document.getElementById('_40_22_icons8-_____-50_2').style.opacity = '1'; // Эффект отжатия
+    }, 100);
+}
+
+document.getElementById('_25_3_tg_1').addEventListener("click", function() {
+    window.location.href = "https://t.me/uidknowal_kiopi";
+});
+
+document.getElementById('_38_14_tg_1').addEventListener("click", function() {
+    window.location.href = "https://t.me/kiopizxc";
+});
+
+document.getElementById('_30_8_tg_1').addEventListener("click", function() {
+    window.location.href = "https://t.me/yenix_era";
+});
+
+document.getElementById('_34_17_icons8-____________-_________-____________-2-50_1').addEventListener("click", function() {
+    window.location.href = "https://music.yandex.ru/users/kiopiplay/playlists/3?utm_medium=copy_link";
+});
+
+document.getElementById('_40_20_icons8-_____-50_1').addEventListener("click", playPause);
+document.getElementById('_40_21_icons8-_____-50_1').addEventListener("click", nextTrack);
+document.getElementById('_40_22_icons8-_____-50_2').addEventListener("click", prevTrack);
+
+document.getElementById('startButton').addEventListener("click", function() {
+    audioPlayer.play().then(() => {
+        document.getElementById('startButton').style.display = 'none'; // Скрыть кнопку после нажатия
+    }).catch(error => {
+        console.error('Произошла ошибка:', error);
+    });
+});
