a89d20baab3982740fc238b812e37d6878299c60
kiop1-art <kiopiplay@ya.ru>
2025-09-02 18:35:34 +0500

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const audioPlayer = document.getElementById('audioPlayer');
 let currentTrackIndex = 0;
+let hasInteracted = false;
+
 const tracks = [
     'audio/track1.mp3',
     'audio/track2.mp3',
@@ -13,20 +15,31 @@ const tracks = [
     'audio/track10.mp3'
 ];
 
+function initAudio() {
+    if (!hasInteracted) {
+        audioPlayer.src = tracks[currentTrackIndex];
+        audioPlayer.play().catch(() => {});
+        hasInteracted = true;
+    }
+}
+
 function playPause() {
-    if (audioPlayer.paused) {
-        audioPlayer.play();
-        document.getElementById('_40_20_icons8-_____-50_1').style.opacity = '0.5';
+    if (hasInteracted) {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+        } else {
+            audioPlayer.pause();
+        }
+        document.getElementById('_40_20_icons8-_____-50_1').style.opacity = audioPlayer.paused ? '1' : '0.5';
     } else {
-        audioPlayer.pause();
-        document.getElementById('_40_20_icons8-_____-50_1').style.opacity = '1';
+        initAudio();
     }
 }
 
 function nextTrack() {
     currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
     audioPlayer.src = tracks[currentTrackIndex];
-    audioPlayer.play();
+    if (hasInteracted) audioPlayer.play();
     document.getElementById('_40_21_icons8-_____-50_1').style.opacity = '0.5';
     setTimeout(() => {
         document.getElementById('_40_21_icons8-_____-50_1').style.opacity = '1';
@@ -36,36 +49,48 @@ function nextTrack() {
 function prevTrack() {
     currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
     audioPlayer.src = tracks[currentTrackIndex];
-    audioPlayer.play();
+    if (hasInteracted) audioPlayer.play();
     document.getElementById('_40_22_icons8-_____-50_2').style.opacity = '0.5';
     setTimeout(() => {
         document.getElementById('_40_22_icons8-_____-50_2').style.opacity = '1';
     }, 100);
 }
 
-document.getElementById('_25_3_tg_1').addEventListener("click", function() {
-    window.location.href = "https://t.me/uidknowal_kiopi";
+audioPlayer.addEventListener('play', () => {
+    document.getElementById('_40_20_icons8-_____-50_1').style.opacity = '0.5';
 });
 
-document.getElementById('_38_14_tg_1').addEventListener("click", function() {
-    window.location.href = "https://t.me/kiopizxc";
+audioPlayer.addEventListener('pause', () => {
+    document.getElementById('_40_20_icons8-_____-50_1').style.opacity = '1';
 });
 
-document.getElementById('_30_8_tg_1').addEventListener("click", function() {
-    window.location.href = "https://t.me/yenix_era";
+document.getElementById('_25_3_tg_1').addEventListener("click", () => {
+    window.open("https://t.me/uidknowal_kiopi", '_blank');
 });
 
-document.getElementById('_34_17_icons8-____________-_________-____________-2-50_1').addEventListener("click", function() {
-    window.location.href = "https://music.yandex.ru/users/(name)/playlists/3?utm_medium=copy_link";
+document.getElementById('_38_14_tg_1').addEventListener("click", () => {
+    window.open("https://t.me/kiopizxc", '_blank');
+});
+
+document.getElementById('_30_8_tg_1').addEventListener("click", () => {
+    window.open("https://t.me/yenix_era", '_blank');
+});
+
+document.getElementById('_34_17_icons8-____________-_________-____________-2-50_1').addEventListener("click", () => {
+    window.open("https://music.yandex.ru/users/kiopi/playlists/3", '_blank');
 });
 
 document.getElementById('_40_20_icons8-_____-50_1').addEventListener("click", playPause);
-document.getElementById('_40_21_icons8-_____-50_1').addEventListener("click", nextTrack);
-document.getElementById('_40_22_icons8-_____-50_2').addEventListener("click", prevTrack);
+document.getElementById('_40_21_icons8-_____-50_1').addEventListener("click", () => {
+    if (!hasInteracted) initAudio();
+    nextTrack();
+});
+document.getElementById('_40_22_icons8-_____-50_2').addEventListener("click", () => {
+    if (!hasInteracted) initAudio();
+    prevTrack();
+});
 
-window.addEventListener('load', () => {
-    audioPlayer.src = tracks[currentTrackIndex];
-    audioPlayer.play().catch(() => {
-        console.('kiopi');
-    });
+document.getElementById('startButton')?.addEventListener("click", function() {
+    initAudio();
+    this.style.display = 'none';
 });
